bf220a106056e1dee41e8579277432d5b84f0335
Ailton Nunes <nunes.tom@gmail.com>
2021-09-06 15:06:51 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,13 +1,13 @@
-import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
-import { AddAccountModel } from '../../../../domain/usecases/add-account'
-import { AccountModel } from '../../../../domain/models/account'
-import { MongoHelper } from '../helpers/mongo-helper'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { AccountModel } from '../../../../domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(accountData)
-    const accountId = result.insertedId
-    return MongoHelper.map(await accountCollection.findOne(accountId))
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const accountId = result.insertedId;
+    return MongoHelper.map(await accountCollection.findOne(accountId));
   }
 }
